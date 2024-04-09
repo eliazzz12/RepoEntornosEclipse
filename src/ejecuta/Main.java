fbc267527bb53b36acb7da9f0785497d30799fa2
Elias Martin <emgdev@protonmail.com>
2024-04-09 08:49:55 +0200

    Crear TreeSet mascotas

--- src/ejecuta/Main.java
+++ src/ejecuta/Main.java
@@ -1,9 +1,15 @@
 package ejecuta;
 
+import java.util.Set;
+import java.util.TreeSet;
+
+import modelo.Animal;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Set<Animal> mascotas = new TreeSet<>();
+		
 	}
 
 }
