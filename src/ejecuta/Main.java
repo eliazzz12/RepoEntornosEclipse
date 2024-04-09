74109f482ca4ec95e12e749e23fd267d12e0d4ef
Elias Martin <emgdev@protonmail.com>
2024-04-09 09:03:12 +0200

    Mejorar mostrarAnimal y añadir dos animales

--- src/ejecuta/Main.java
+++ src/ejecuta/Main.java
@@ -4,22 +4,23 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import modelo.Animal;
+import modelo.Gato;
+import modelo.Perro;
 
 public class Main {
 	static Set<Animal> mascotas = new TreeSet<>();
 
 	public static void main(String[] args) {
-		
+		mascotas.add(new Perro("Pluto", 7, "153AW", "Labrador"));
+		mascotas.add(new Gato("Silvestre", 4, "627FB", "Negro"));
 	}
 
 	private static void mostrarAnimal(Animal animal) {
-			if(existe(animal)) {
-				System.out.println("Nombre:"+animal.getNombre());
-				System.out.println("Raza:"+animal.getRaza());
-				System.out.println("Edad:"+animal.getEdad());
-				System.out.println("Chip:"+animal.getChip());
-				return;
-			}
-		System.out.println("No tenemos ese animal :(");
+		System.out.println();
+		System.out.println("Nombre:"+animal.getNombre());
+		System.out.println("Raza:"+animal.getRaza());
+		System.out.println("Edad:"+animal.getEdad());
+		System.out.println("Chip:"+animal.getChip());
 	}
+	
 }
